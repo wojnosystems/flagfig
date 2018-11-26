9373585f72167b56fedb3c7fb7e33c49f6cf99b2
Chris Wojno <chris.wojno@hsgp.org>
2018-11-25 23:09:22 -0800

    Added the license

--- flagfig/flagfig.go
+++ flagfig/flagfig.go
@@ -1,4 +1,15 @@
-// Copyright 2018 Chris Wojno. All rights reserved.
+/*
+Copyright 2018 Chris Wojno.
+Attribution 4.0 International (CC BY 4.0)
+All rights reserved.
+You do not have to comply with the license for elements of the material in the public domain or where your use is
+permitted by an applicable exception or limitation.
+
+No warranties are given. The license may not give you all of the permissions necessary for your intended use.
+For example, other rights such as publicity, privacy, or moral rights may limit how you use the material.
+
+See LICENSE file for the full license
+*/
 
 /*
 	Package flagfig implements configuration file, environment, and command-line flag parsing
